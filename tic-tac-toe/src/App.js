ff4ce1e191ae12814def5071ec789bcac528b31b
Rafael Silvestre de Oliveira <rafael.oliveira@philips.com>
2021-09-26 00:06:15 -0300

    Created the Board component with the structure of the board and nothing else, exported and imported into the app

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import Board from './Board'
 
 export default function App() {
   return (
-    <>
-      <h1>
-        Teste
-      </h1>
-    </>
+      <Board>
+        
+      </Board>
   );
 }
---
+++ tic-tac-toe/src/Board.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './board.css'
+
+export default function Board() {
+  return (
+    <>
+      <table className="bordaSimples dimensaoPadrao">
+      <tr>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+        </tr>
+        <tr>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+        </tr>
+        <tr>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+          <td>
+            
+          </td>
+        </tr>
+      </table>
+    </>
+  );
+}
