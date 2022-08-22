31488c88920936f68e6145a06a415a818284e6f1
Rafael Silvestre De Oliveira <rafael.silvestre@ambevtech.com.br>
2022-08-22 14:17:30 -0300

    added a counter and random numbers

--- app-for-testing/src/App.tsx
+++ app-for-testing/src/App.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+
+function randomizedNumber() : number {
+  return Math.round((Math.random() * 10));
+}
+
 function App() {
-  const value = 'Teste 1';
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    
+    setFirstNumber(randomizedNumber());
+    setSecondNumber(randomizedNumber());
+    setCount(count + 1);
+
+  },[firstNumber, secondNumber]);
 
   return (
     <div>
       <div className="row">
-        <div className="col-md-6">{value}</div>
-        <div className="col-md-6">Teste 2</div>
+        <div className="col-md-12">
+          How many times do we need to run a random number generator function in order to get the same two two-digit numbers twice in a row? Check 'Count' number to know.
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">First Number: {firstNumber}</div>
+        <div className="col-md-4">Second Number: {secondNumber}</div>
+        <div className="col-md-4">Count: {count}</div>
       </div>
     </div>
   );
 }
 
 export default App;
+
