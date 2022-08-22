0df9b48db4764406a4814156342a81dffdb3a6da
Rafael Silvestre De Oliveira <rafael.silvestre@ambevtech.com.br>
2022-08-22 08:14:44 -0300

    Adding prettier, its config file + applying it to the codebase

--- tic-tac-toe/src/Xis.js
+++ tic-tac-toe/src/Xis.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Xis() {
-    return(
-        <svg height="100" width="100">
-            <line x1="10" y1="10" x2="80" y2="80" strokeWidth="20" stroke="blue" />
-            <line x1="80" y1="10" x2="10" y2="80" strokeWidth="20" stroke="blue" />
-        </svg>
-    )
+  return (
+    <svg height="100" width="100">
+      <line x1="10" y1="10" x2="80" y2="80" strokeWidth="20" stroke="blue" />
+      <line x1="80" y1="10" x2="10" y2="80" strokeWidth="20" stroke="blue" />
+    </svg>
+  );
 }
