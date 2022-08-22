dec73b1d8f2964a70ed8cd17abb2e7c798032ea9
Rafael Silvestre De Oliveira <rafael.silvestre@ambevtech.com.br>
2022-08-22 10:41:46 -0300

    added prettier

--- app-for-testing/src/App.tsx
+++ app-for-testing/src/App.tsx
@@ -1,18 +1,13 @@
 import './App.css';
 
 function App() {
-
   const value = 'Teste 1';
 
   return (
     <div>
-      <div className='row'>
-        <div className='col-md-6'>
-          {value}
-        </div>
-        <div className='col-md-6'>
-          Teste 2
-        </div>
+      <div className="row">
+        <div className="col-md-6">{value}</div>
+        <div className="col-md-6">Teste 2</div>
       </div>
     </div>
   );
