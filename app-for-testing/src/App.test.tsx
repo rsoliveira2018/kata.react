e42a3bf8c08d4020d9fde60f9d84ed5c0a1cabf0
Rafael Silvestre De Oliveira <rafael.silvestre@ambevtech.com.br>
2022-08-26 09:32:02 -0300

    added totalCount and average + some styling and removed some unneeded files and css styles + fixed test name and functionality

--- app-for-testing/src/App.test.tsx
+++ app-for-testing/src/App.test.tsx
@@ -2,8 +2,8 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Run Again button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Teste 1/i);
+  const linkElement = screen.getByText(/Run Again/i);
   expect(linkElement).toBeInTheDocument();
 });
--- app-for-testing/src/App.tsx
+++ app-for-testing/src/App.tsx
@@ -1,33 +1,79 @@
 import { useEffect, useState } from "react";
 
 function randomizedNumber() : number {
-  return Math.round((Math.random() * 99));
+  return Math.round((Math.random() * 2));
 }
 
 function App() {
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
   const [count, setCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const [times, setTimes] = useState(1);
+  const [average, setAverage] = useState(0);
 
   useEffect(() => {
     
     setFirstNumber(randomizedNumber());
     setSecondNumber(randomizedNumber());
     setCount(count + 1);
+    setTotalCount(totalCount + 1);
+    calculateAverage();
 
   },[firstNumber, secondNumber]);
+
+  function runAgain(): void{
+    setCount(0);
+    setTimes(times + 1);
+    setFirstNumber(0);
+    setSecondNumber(0);
+  }
+
+  function calculateAverage(): void {
+    setAverage(Math.round((totalCount + 1) / times));
+  }
 
   return (
     <div>
       <div className="row">
-        <div className="col-md-12">
-          How many times do we need to run a random number generator function in order to get the same two two-digit numbers twice in a row? Check 'Count' number to know.
+        <div className="col-md-2 fullFillTheCol"></div>
+        <div className="col-md-8">
+          <h6 className="text-primary">
+            <br/>
+            How many times do we need to run a random number generator function in order to get the same two two-digit numbers twice in a row?
+            <br/>
+            Check 'Count' number to know the last one and 'Average' for the average amongst all your previous runs
+            <br/> 
+            <br/>
+            <span className="text-danger">
+              Do not Refresh the page, otherwise you will reset all the countings and average.
+              <br/>
+              <br/>
+            </span>
+          </h6>
         </div>
+        <div className="col-md-2 fullFillTheCol"></div>
       </div>
       <div className="row">
-        <div className="col-md-4">First Number: {firstNumber}</div>
-        <div className="col-md-4">Second Number: {secondNumber}</div>
-        <div className="col-md-4">Count: {count}</div>
+        <div className="col-md-2 fullFillTheCol"></div>
+        <div className="col-md-2">First Number: {firstNumber}</div>
+        <div className="col-md-2">Second Number: {secondNumber}</div>
+        <div className="col-md-2">Count: {count}</div>
+        <div className="col-md-2">Average: {average}</div>
+        <div className="col-md-2 fullFillTheCol"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-2 fullFillTheCol"></div>
+        <div className="col-md-8">
+          <br/>
+          <button onClick={runAgain}>Run Again</button>
+        </div>
+        <div className="col-md-2 fullFillTheCol"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-2 fullFillTheCol"></div>
+        <div className="col-md-8"><br/></div>
+        <div className="col-md-2 fullFillTheCol"></div>
       </div>
     </div>
   );
